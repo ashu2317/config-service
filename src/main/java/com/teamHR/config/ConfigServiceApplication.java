91f86c308ca53ba74ab57d041e9a3c1cf8be007c
ashu2317 <agate.ashu@gmail.com>
2016-06-17 17:47:55 +0400

    Added encrypted password

--- src/main/java/com/teamHR/config/ConfigServiceApplication.java
+++ src/main/java/com/teamHR/config/ConfigServiceApplication.java
@@ -1,12 +1,16 @@
 package com.teamHR.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.environment.EncryptableEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
